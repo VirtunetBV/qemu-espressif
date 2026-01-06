esp32-open-mac/qemu

Implement undocument wifi registers, get AP mode working
ESP32 as client, QEMU as AP works with the following command:

build/qemu-system-xtensa -nic user,model=misc.esp32_wifi,id=u1 -no-reboot -S -s -nographic -machine esp32 -drive file=../http_request/meshtest_http_10.0.0.8.bin,if=mtd,format=raw -object filter-dump,id=f1,file=dump.pcap,netdev=u1 -drive file=../efuse.bin,if=none,format=raw,id=efuse -global driver=nvram.esp32.efuse,property=drive,value=efuse

The efuse file was dumped from a real ESP32 with espefuse.py dump

The meshtest_http_... file was built from
esp-idf/examples/protocols/http_request, configured to connect to
one of the APs hardcoded in hw/misc/esp32_wifi_ap.c

Co-authored-by: Martin Johnson <M.J.Johnson@massey.ac.nz>

hw/xtensa/esp32_intc.c
@@ -25,6 +25,7 @@
 static void esp32_intmatrix_irq_handler(void *opaque, int n, int level)
 {
     Esp32IntMatrixState *s = ESP32_INTMATRIX(opaque);
+    s->irq_raw[n] = level;
     for (int i = 0; i < ESP32_CPU_COUNT; ++i) {
         if (s->outputs[i] == NULL) {
             continue;
@@ -61,10 +62,20 @@ static uint64_t esp32_intmatrix_read(void* opaque, hwaddr addr, unsigned int siz
 static void esp32_intmatrix_write(void* opaque, hwaddr addr, uint64_t value, unsigned int size)
 {
     Esp32IntMatrixState *s = ESP32_INTMATRIX(opaque);
-    uint8_t* map_entry = get_map_entry(s, addr);
+    int source_index = (addr / sizeof(uint32_t)) % ESP32_INT_MATRIX_INPUTS;
+    uint8_t *map_entry = get_map_entry(s, addr);
+    if (value == INTMATRIX_UNINT_VALUE) {
+        int si = s->irq_raw[source_index];
+        esp32_intmatrix_irq_handler(s, source_index, 0);
+        s->irq_raw[source_index] = si;
+    }
     if (map_entry != NULL) {
         *map_entry = value & 0x1f;
     }
+    if (value != INTMATRIX_UNINT_VALUE && s->irq_raw[source_index]) {
+        esp32_intmatrix_irq_handler(s, source_index, 1);
+    }
+
 }
 
 static const MemoryRegionOps esp_intmatrix_ops = {
@@ -76,6 +87,7 @@ static const MemoryRegionOps esp_intmatrix_ops = {
 static void esp32_intmatrix_reset(DeviceState *dev)
 {
     Esp32IntMatrixState *s = ESP32_INTMATRIX(dev);
+    memset(s->irq_raw, 0, sizeof(s->irq_raw));
     memset(s->irq_map, INTMATRIX_UNINT_VALUE, sizeof(s->irq_map));
     for (int i = 0; i < ESP32_CPU_COUNT; ++i) {
         if (s->outputs[i] == NULL) {

VirtunetBV/qemu-espressif

hw/xtensa/esp32_intc.c
@@ -25,6 +25,7 @@
 static void esp32_intmatrix_irq_handler(void *opaque, int n, int level)
 {
     Esp32IntMatrixState *s = ESP32_INTMATRIX(opaque);
+    s->irq_raw[n] = level;
     for (int i = 0; i < ESP32_CPU_COUNT; ++i) {
         if (s->outputs[i] == NULL) {
             continue;
@@ -61,9 +62,18 @@ static uint64_t esp32_intmatrix_read(void* opaque, hwaddr addr, unsigned int siz
 static void esp32_intmatrix_write(void* opaque, hwaddr addr, uint64_t value, unsigned int size)
 {
     Esp32IntMatrixState *s = ESP32_INTMATRIX(opaque);
-    uint8_t* map_entry = get_map_entry(s, addr);
+    int source_index = (addr / sizeof(uint32_t)) % ESP32_INT_MATRIX_INPUTS;
+    uint8_t *map_entry = get_map_entry(s, addr);
+    if (value == INTMATRIX_UNINT_VALUE) {
+        int raw = s->irq_raw[source_index];
+        esp32_intmatrix_irq_handler(s, source_index, 0);
+        s->irq_raw[source_index] = raw;
+    }
     if (map_entry != NULL) {
         *map_entry = value & 0x1f;
+    }
+    if (value != INTMATRIX_UNINT_VALUE && s->irq_raw[source_index]) {
+        esp32_intmatrix_irq_handler(s, source_index, 1);
     }
 }
 
@@ -76,6 +86,7 @@ static const MemoryRegionOps esp_intmatrix_ops = {
 static void esp32_intmatrix_reset_hold(Object *obj, ResetType type)
 {
     Esp32IntMatrixState *s = ESP32_INTMATRIX(obj);
+    memset(s->irq_raw, 0, sizeof(s->irq_raw));
     memset(s->irq_map, INTMATRIX_UNINT_VALUE, sizeof(s->irq_map));
     for (int i = 0; i < ESP32_CPU_COUNT; ++i) {
         if (s->outputs[i] == NULL) {